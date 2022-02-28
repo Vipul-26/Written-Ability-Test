6679fd7c004501026ebee84862be17ef08269b79
Vipul-26 <vipulsinghssm1@gmail.com>
2022-02-28 12:13:25 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import ButtonComponent from "./Components/buttoncomponent";
+import PracticeSet from "./Components/practiceSet";
+
+export default function App() {
+  let [currentIndex, setCurrentIndex] = useState(0);
+  return (
+    <div className="App">
+      <Router>
+        <Navbar setCurrentIndex={setCurrentIndex} currentIndex={currentIndex} />
+        <Switch>
+          <Route exact path="/" component={ButtonComponent} />
+          <Route
+            exact
+            path="/set:id"
+            render={(props) => (
+              <PracticeSet
+                {...props}
+                currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+              />
+            )}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Timer from "./timer";
+import bell_tannn from "./../asset/bell_tannn.mpeg";
+import { Route } from "react-router-dom";
+function NavBar({ setCurrentIndex, currentIndex }) {
+  let ringBell = () => {
+    let snd = new Audio(bell_tannn);
+    snd.play();
+  };
+  return (
+    <Navbar className="justify-content-between" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">W.A.T. Questions</Navbar.Brand>
+      <Nav.Item>
+        <Route path="/set:id">
+          <Timer
+            ringBell={ringBell}
+            setCurrentIndex={setCurrentIndex}
+            currentIndex={currentIndex}
+          />
+        </Route>
+      </Nav.Item>
+    </Navbar>
+  );
+}
+
+export default NavBar;
